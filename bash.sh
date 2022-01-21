0f3fb23c9eac640f5430aac4e319fd88ac16ad73
Ilnur Fatkullin <ilnur.fatkulllin@gmail.com>
2022-01-21 16:55:52 +0500

    change bash and compose

--- bash.sh
+++ bash.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sleep 10
 su - postgres -c "pg_basebackup --host=masterPG --username=postgres --pgdata=/data/back -X stream --write-recovery-conf"
 su - postgres -c "/usr/lib/postgresql/13/bin/pg_ctl start -D /data/back"
 sleep infinity
